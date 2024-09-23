4dda7df3521ff4558608dba30a57edd2acece703
몽현 정 <wjdahdgus12@naver.com>
2024-09-24 01:12:03 +0900

    feat : StepScroll 구현

--- Assets/Map.cs
+++ Assets/Map.cs
@@ -1,14 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Map : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    GameObject block;
+    
+    [SerializeField]
+    GameObject block2;
+
+    [SerializeField]
+    public int scrollPower;
+    public int mapScrollPower;
+    public Image image;
 
     public void Scroll()
     {
-        gameObject.transform.position = Vector3.down * 5;
+        if (block.transform.position.y <= -12)
+            block.transform.position = new Vector3(block.transform.position.x, 12, block.transform.position.z);
+    
+
+      if (block2.transform.position.y <= -24)
+            block2.transform.position = new Vector3(block2.transform.position.x, 0, block2.transform.position.z);
+
+
+
+        gameObject.transform.position += Vector3.down * scrollPower;
+        image.transform.position += Vector3.down * mapScrollPower;
+
+
     }
 
     void Start()
