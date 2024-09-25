ae5d58e7b1770ae80dc0e3169d27570926a45d0a
몽현 정 <wjdahdgus12@naver.com>
2024-09-26 00:05:08 +0900

    fix : 뒷배경 수정

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -15,7 +15,10 @@ public class Map : MonoBehaviour
     [SerializeField]
     public int scrollPower;
 
-    public Image image;
+    public Image image1;
+    public Image image2;
+    public Image image3;
+
     public Sprite sprite;
     TouchtoScreen touchtoScreen;
     SpriteRenderer spriteRenderer;
@@ -33,9 +36,7 @@ public class Map : MonoBehaviour
       if (block2.transform.position.y <= -24)
             block2.transform.position = new Vector3(block2.transform.position.x, 0, block2.transform.position.z);
 
-        if (touchtoScreen.Point >= 10)
-            image.sprite = sprite;
-
+     
 
         block.transform.position += Vector3.down * scrollPower;
         block2.transform.position += Vector3.down * scrollPower;
@@ -55,9 +56,13 @@ public class Map : MonoBehaviour
         while (gameOver)
         {
             yield return new WaitForSeconds(2);
-            image.color = Color.blue;
+            image1.color = new Color(image1.color.r,image1.color.g,image1.color.b,0.5f);
+            image2.color = new Color(image2.color.r, image2.color.g, image2.color.b, 0.5f);
+            image3.color = new Color(image3.color.r, image3.color.g, image3.color.b, 0.5f);
             yield return new WaitForSeconds(0.3f);
-            image.color = Color.white;
+            image1.color = new Color(image1.color.r, image1.color.g, image1.color.b,0);
+            image2.color = new Color(image2.color.r, image2.color.g, image2.color.b,0);
+            image3.color = new Color(image3.color.r, image3.color.g, image3.color.b,0);
         }
     }
 
