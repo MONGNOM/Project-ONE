071464c61074e9faa141f9380e5268d9d9b11e87
몽현 정 <wjdahdgus12@naver.com>
2024-10-13 23:57:27 +0900

    feat : Hp Monster추가

---
+++ Assets/Scripts/HpMonster.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpMonster : MonoBehaviour
+{
+    // hp monster 점수따라 생김새에 속도가 달라짐 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
