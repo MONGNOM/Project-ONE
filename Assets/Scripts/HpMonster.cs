20f32e32bf432396a34f25e11cd30f89834c2054
몽현 정 <wjdahdgus12@naver.com>
2024-10-14 00:00:01 +0900

    feat : movetime 추가

--- Assets/Scripts/HpMonster.cs
+++ Assets/Scripts/HpMonster.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HpMonster : MonoBehaviour
 {
     // hp monster 점수따라 생김새에 속도가 달라짐 
 
+    private float movetime = 1;
+
+    IEnumerator MoveHpMonster()
+    {
+        yield return new WaitForSeconds(movetime);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        StartCoroutine(MoveHpMonster());
     }
 
     // Update is called once per frame
