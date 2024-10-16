751ee46a376473d56568840cc2b92ef380f773cd
몽현 정 <wjdahdgus12@naver.com>
2024-10-17 00:06:50 +0900

    feat : MonsterHp DelegateChain 추가

--- Assets/Scripts/HpMonster.cs
+++ Assets/Scripts/HpMonster.cs
@@ -2,21 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HpMonster : MonoBehaviour
 {
     // 몬스터는 공격만 받고 어느 일정 시간동안 깨지 못한다면 타워를 한방에 깨버리는 판정
     // hp에 총알 데미지를 대입해서 데미지를 받는싱그로 진행
 
+
+
     private float monsterHp = 1000; // 점수에 맞게 설정해주는게 맞을듯
+
+    public float MonsterHp { get { return monsterHp; } private set { monsterHp = value; changeHp?.Invoke(monsterHp); } }
+    public delegate void ChangeMonsterHp(float hp);
+    ChangeMonsterHp changeHp;
+
+
     private float attackTime = 1;
     private float attackDamage = 1; // 타워 체력마늨 
+
+    [SerializeField]
+    private Image hpImage;
+    
+    [SerializeField]
+    private Image attackImage;
 
 
 
     IEnumerator MoveHpMonster()
     {
-        // monsterhp;    
+        // attackImage;    
         // ㄴ 함수로 빼내서 조건문걸고 조건 통과하면 hp 및 데미지 변경으로 해줘야 쓸모없는 코드 안탈듯
         yield return new WaitForSeconds(attackTime);
     }
@@ -29,7 +44,9 @@ public class HpMonster : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        monsterHp = 1000;
         StartCoroutine(MoveHpMonster());
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
