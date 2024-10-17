fde41e983760f98970e0bf4d5c8879f64d9d2940
몽현 정 <wjdahdgus12@naver.com>
2024-10-17 18:57:27 +0900

    feat : GameOver 추가

--- Assets/GameOverPage.cs
+++ Assets/GameOverPage.cs
@@ -2,24 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class GameOverPage : MonoBehaviour
 {
-
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
 
     public void GameExit()
     {
         Application.Quit();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
